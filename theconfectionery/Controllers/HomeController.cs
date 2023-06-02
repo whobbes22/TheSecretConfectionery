8de6f253a22f58bb29426aece611186d0a107ac0
Joe W <whobbes22@gmail.com>
2023-06-02 09:33:20 -0700

    added home page

---
+++ theconfectionery/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using TheConfectionery.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace TheConfectionery.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      private readonly TheConfectioneryContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public HomeController(UserManager<ApplicationUser> userManager, TheConfectioneryContext db)
+      {
+        _userManager = userManager;
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        Treat[] treats = _db.Treats.ToArray();
+        Flavor[] flavors = _db.Flavors.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("treats", treats);
+        model.Add("flavors", flavors);
+        return View(model);
+      }      
+
+    }
+}
