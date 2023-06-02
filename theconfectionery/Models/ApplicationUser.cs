ce6d56bcecbf68d95fd970cd5c8894d54f9ee67a
Joe W <whobbes22@gmail.com>
2023-06-02 08:44:10 -0700

    added actual database

---
+++ theconfectionery/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace TheConfectionery.Models
+{
+  public class ApplicationUser : IdentityUser
+  {
+    
+  }
+}
--- theconfectionery/Models/TheConfectioneryContext.cs
+++ theconfectionery/Models/TheConfectioneryContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace FantacyRecipe.Models
+namespace TheConfectionery.Models
 {
   public class TheConfectioneryContext : IdentityDbContext<ApplicationUser>
   {
